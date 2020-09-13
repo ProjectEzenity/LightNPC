e6a92609c902f03106973c7efd2882ac8efdbc47
ezenity <anthonymmacallister@gmail.com>
2020-09-13 00:42:27 -0400

    Feat: Added class -> NullSocket

---
+++ src/main/java/com/ezenity/lightnpc/util/nms/NullSocket.java
@@ -0,0 +1,25 @@
+package com.ezenity.lightnpc.util.nms;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author martin
+ * @version 0.0.1
+ */
+public class NullSocket extends Socket {
+    @Override
+    public InputStream getInputStream() {
+        byte[] buf = new byte[5];
+        return new ByteArrayInputStream(buf);
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+        return new ByteArrayOutputStream();
+    }
+}
